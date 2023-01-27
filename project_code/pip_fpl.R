513db05bfbc1140b2324537d3f4f4e420b350bce
danjwalton <20973540+danjwalton@users.noreply.github.com>
2023-01-27 12:49:01 +0000

    Add comments to script

--- project_code/pip_fpl.R
+++ project_code/pip_fpl.R
@@ -1,54 +1,75 @@
+#Install required packages
 required.packages <- c("data.table","jsonlite", "rstudioapi", "httr")
 install.packages(required.packages[!(required.packages %in% installed.packages())])
 lapply(required.packages, require, character.only=T)
+
+#Set working directory as local folder
 setwd(dirname(getActiveDocumentContext()$path))
 
+#Read WUP population function from remote repo
 source("https://raw.githubusercontent.com/devinit/gha_automation/main/general/wupData.R")
 
+#Set up API base address
 pip <- "https://api.worldbank.org/pip/v1/pip?"
 
+#Read in food poverty lines
 fpls <- fread("fpl_2023.csv")
+
+#Remove countries with no FPL for 2017
 fpls <- fpls[!is.na(fpl_2017)]
 
+#Melt to 'long' format
 fpls <- melt(fpls, id.vars = "countrycode", variable.factor = F)
 
+#Extract the reporting year (for API use), and effective year
 fpls[, `:=` (reporting_year = substr(variable, 5, 8), effective_year = substr(variable, nchar(variable) - 3, nchar(variable)))]
 
+#Either read or create the output file
 if(length(list.files(pattern = "^food_poverty_2023[.]csv$")) == 1){
   fpl_out <- fread("food_poverty_2023.csv")
 } else {
   fpl_out <- data.table(cc = character(), effective_year = integer())
-  }
+}
 
+#List the FPLs which are outstanding to do based on the read output file
 fpls_todo <- fpls[!(paste0(countrycode, effective_year) %in% fpl_out[, paste0(cc, effective_year)])]
 
+#Iterate through the outstanding FPLs
 for(i in 1:nrow(fpls_todo)){
   if(exists("fpl_response")) rm(fpl_response)
   
+  #Read parameters for the API call from the list of FPLs
   fpl_r <- fpls_todo[i]
   cc <- fpl_r$countrycode
   rep_lvl <- "all"
   reporting_year <- fpl_r$reporting_year
   pov_line <- round(fpl_r$value, 3)
   
+  #Message country code and year to console to indicate progress
   message(cc, fpl_r$effective_year)
   
+  #Select subnational data when required
   if(nchar(cc) > 3){
     
     rep_lvl <- ifelse(substr(cc, 5, 5) == "R", "rural", "urban")
     cc <- substr(cc, 0, 3)
   }
   
+  #Create the API call address based on parameters
   pip_call <- paste0(pip, "country=", cc, "&year=", reporting_year, "&povline=", pov_line, "&reporting_level=", rep_lvl, "&fill_gaps=true")
   
+  #Check whether the call is valid
   if(status_code(GET(pip_call)) != 404){
   
+    #If valid, read the data from it
     fpl_response <- data.table(fromJSON(pip_call))
   }
   
+  #Fill in the effective year and original country code to the outputted data
   fpl_response$effective_year <- fpl_r$effective_year
   fpl_response$cc <- fpl_r$countrycode
 
+  #Append the outputted data to the output table and write it to the local folder
   fpl_out <- rbind(fpl_out, fpl_response, fill = T)
   fwrite(fpl_out, "food_poverty_2023.csv")
 }
